16886ac1e487c7324a5f274df4eb7047f95880b6
Jeroen van Maanen <jeroen@sollunae.net>
2025-10-26 21:40:29 +0000

    Refactored error handling

--- src/error.rs
+++ src/error.rs
@@ -1,64 +1,78 @@
 ///! Error structs for iroh-persist
 use std::path::PathBuf;
 
-use iroh::KeyParsingError;
+use iroh::{KeyParsingError, SecretKey};
 use snafu::Snafu;
 
 #[derive(Debug, Snafu)]
 #[non_exhaustive]
 pub enum PersistError {
+    KeyWriteError {
+        source: KeyWriteErrorSource,
+        key: SecretKey,
+    },
+
+    #[snafu(transparent)]
+    KeyReadError { source: KeyReadErrorSource },
+}
+
+pub fn reading_file(file: PathBuf) -> impl FnOnce(std::io::Error) -> PersistError {
+    |e| PersistError::KeyReadError {
+        source: KeyReadErrorSource::ReadFileError { source: e, file },
+    }
+}
+
+pub fn writing_file(file: PathBuf) -> impl FnOnce(std::io::Error) -> KeyWriteErrorSource {
+    |e| KeyWriteErrorSource::WriteFileError { source: e, file }
+}
+
+#[derive(Debug, Snafu)]
+#[non_exhaustive]
+pub enum KeyReadErrorSource {
+    #[snafu(transparent)]
+    IOError {
+        source: std::io::Error,
+    },
+
+    ReadFileError {
+        source: std::io::Error,
+        file: PathBuf,
+    },
+
+    #[snafu(transparent)]
+    SshParsingError {
+        source: ssh_key::Error,
+    },
+
+    #[snafu(transparent)]
+    IrohParsingError {
+        source: iroh::KeyParsingError,
+    },
+
+    InvalidKeyTypeError {
+        algorithm: Option<String>,
+    },
+}
+
+impl From<KeyParsingError> for PersistError {
+    fn from(source: KeyParsingError) -> Self {
+        PersistError::KeyReadError {
+            source: KeyReadErrorSource::IrohParsingError { source },
+        }
+    }
+}
+
+#[derive(Debug, Snafu)]
+#[non_exhaustive]
+pub enum KeyWriteErrorSource {
     #[snafu(transparent)]
     IOError { source: std::io::Error },
 
-    FileError {
+    WriteFileError {
         source: std::io::Error,
         file: PathBuf,
     },
 
     #[snafu(transparent)]
     KeyEncodeError { source: ssh_key::Error },
-
-    #[snafu(transparent)]
-    KeyDecodeError { source: KeyDecodeErrorSource },
-}
-
-pub fn for_file(file: PathBuf) -> impl FnOnce(std::io::Error) -> PersistError {
-    return |e| PersistError::FileError { source: e, file };
-}
-
-#[derive(Debug, Snafu)]
-#[non_exhaustive]
-pub enum KeyDecodeErrorSource {
-    #[snafu(transparent)]
-    SshParsing {
-        source: ssh_key::Error,
-    },
-
-    #[snafu(transparent)]
-    IrohParsing {
-        source: iroh::KeyParsingError,
-    },
-
-    InvalidKeyType {
-        algorithm: Option<String>,
-    },
-}
-
-impl PersistError {
-    pub fn ssh_parsing_error(source: ssh_key::Error) -> Self {
-        PersistError::KeyDecodeError {
-            source: KeyDecodeErrorSource::SshParsing { source },
-        }
-    }
-    pub fn ssh_serializing_error(source: ssh_key::Error) -> Self {
-        PersistError::KeyEncodeError { source }
-    }
-}
-
-impl From<KeyParsingError> for PersistError {
-    fn from(source: KeyParsingError) -> Self {
-        PersistError::KeyDecodeError {
-            source: KeyDecodeErrorSource::IrohParsing { source },
-        }
-    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,34 +7,75 @@ use tokio::{
     fs::{self, OpenOptions},
     io::AsyncWriteExt,
 };
-use tracing::log::{debug, error};
+use tracing::{debug, error, warn};
 
 pub mod error;
 
 pub use crate::error::*;
 
-pub async fn get_secret_key(persist_at: Option<PathBuf>) -> SecretKey {
-    get_secret_key_from_ref(persist_at.as_ref()).await
+pub async fn get_secret_key(persist_at: PathBuf) -> SecretKey {
+    get_secret_key_from_option_ref(Some(&persist_at)).await
 }
 
-pub async fn get_secret_key_from_ref(mut persist_at: Option<&PathBuf>) -> SecretKey {
-    match read_key(persist_at).await {
-        Ok(Some(result)) => return result,
-        Ok(None) => (),
-        Err(error) => {
-            error!("Error reading persisted {persist_at:?} key: [{error:?}]");
-            persist_at = None; // Don't overwrite the key that we couln't read
+pub async fn get_secret_key_from_ref(persist_at: &PathBuf) -> SecretKey {
+    get_secret_key_from_option_ref(Some(persist_at)).await
+}
+
+pub async fn get_secret_key_from_option(persist_at: Option<PathBuf>) -> SecretKey {
+    get_secret_key_from_option_ref(persist_at.as_ref()).await
+}
+
+pub async fn get_secret_key_from_option_ref(persist_at: Option<&PathBuf>) -> SecretKey {
+    let key_option = match try_get_secret_key_from_option_ref(persist_at).await {
+        Ok(secret_key) => return secret_key,
+        Err(PersistError::KeyReadError { source }) => {
+            error!("Error reading persisted {persist_at:?} key: [{source:?}]");
+            None
+        }
+        Err(PersistError::KeyWriteError { source, key }) => {
+            error!("Error writing persisted {persist_at:?} key: [{source:?}]");
+            Some(key)
         }
     };
+    warn!("Falling back to ephemeral key");
+    key_option.unwrap_or_else(generate_key)
+}
 
-    let key = SecretKey::generate(&mut rand::rng());
-    debug!("Generate new key: {key:?}");
+pub async fn try_get_secret_key(persist_at: PathBuf) -> Result<SecretKey, PersistError> {
+    try_get_secret_key_from_option_ref(Some(&persist_at)).await
+}
+
+pub async fn try_get_secret_key_from_ref(persist_at: &PathBuf) -> Result<SecretKey, PersistError> {
+    try_get_secret_key_from_option_ref(Some(persist_at)).await
+}
+
+pub async fn try_get_secret_key_from_option(
+    persist_at: Option<PathBuf>,
+) -> Result<SecretKey, PersistError> {
+    try_get_secret_key_from_option_ref(persist_at.as_ref()).await
+}
+
+pub async fn try_get_secret_key_from_option_ref(
+    persist_at: Option<&PathBuf>,
+) -> Result<SecretKey, PersistError> {
     if let Some(node_path) = persist_at {
-        if let Err(error) = write_key(&node_path, &key).await {
-            error!("Could not persist {persist_at:?} key: {node_path:?}: {error:?}");
-        }
+        if let Some(result) = read_key(node_path).await? {
+            return Ok(result);
+        };
+
+        let key = generate_key();
+        write_key(&node_path, &key).await?;
+        Ok(key)
+    } else {
+        warn!("No key path; falling back to ephemeral key");
+        Ok(generate_key())
     }
-    key
+}
+
+pub fn generate_key() -> SecretKey {
+    let result = SecretKey::generate(&mut rand::rng());
+    debug!("Generated new key");
+    result
 }
 
 pub fn default_persist_at<S: AsRef<str>>(app_name: S) -> Option<PathBuf> {
@@ -46,53 +87,60 @@ pub fn default_persist_at<S: AsRef<str>>(app_name: S) -> Option<PathBuf> {
     })
 }
 
-async fn read_key(key_path_option: Option<&PathBuf>) -> Result<Option<SecretKey>> {
-    if let Some(key_path) = key_path_option {
-        if !key_path.exists() {
-            debug!("Secret key not found: {:?}", &key_path);
-            return Ok(None);
-        }
-        let keystr = tokio::fs::read_to_string(key_path)
-            .await
-            .map_err(for_file(key_path.clone()))?;
-        let ser_key = ssh_key::private::PrivateKey::from_openssh(keystr)
-            .map_err(PersistError::ssh_parsing_error)?;
-        let ssh_key::private::KeypairData::Ed25519(kp) = ser_key.key_data() else {
-            let key_algorithm = ser_key.key_data().algorithm().ok();
-            let algorithm = key_algorithm
-                .as_ref()
-                .map(Algorithm::as_str)
-                .map(ToString::to_string);
-            return Err(KeyDecodeErrorSource::InvalidKeyType { algorithm }.into());
-        };
-        let key = SecretKey::from_bytes(&kp.private.to_bytes());
-        debug!("Read key from {key_path:?}");
-        Ok(Some(key))
-    } else {
-        Ok(None)
+async fn read_key(key_path: &PathBuf) -> Result<Option<SecretKey>, PersistError> {
+    if !key_path.exists() {
+        debug!("Secret key not found: {:?}", &key_path);
+        return Ok(None);
     }
+    let keystr = tokio::fs::read_to_string(key_path)
+        .await
+        .map_err(reading_file(key_path.clone()))?;
+    let ser_key = ssh_key::private::PrivateKey::from_openssh(keystr)
+        .map_err(KeyReadErrorSource::from)
+        .map_err(PersistError::from)?;
+    let ssh_key::private::KeypairData::Ed25519(kp) = ser_key.key_data() else {
+        let key_algorithm = ser_key.key_data().algorithm().ok();
+        let algorithm = key_algorithm
+            .as_ref()
+            .map(Algorithm::as_str)
+            .map(ToString::to_string);
+        return Err(KeyReadErrorSource::InvalidKeyTypeError { algorithm }.into());
+    };
+    let key = SecretKey::from_bytes(&kp.private.to_bytes());
+    debug!("Read key from {key_path:?}");
+    Ok(Some(key))
 }
 
-async fn write_key(key_path: &Path, secret_key: &SecretKey) -> Result<()> {
+async fn write_key(key_path: &Path, secret_key: &SecretKey) -> Result<(), PersistError> {
     let ckey = ssh_key::private::Ed25519Keypair {
         public: secret_key.public().as_verifying_key().into(),
         private: secret_key.as_signing_key().into(),
     };
     let ser_key = ssh_key::private::PrivateKey::from(ckey)
         .to_openssh(ssh_key::LineEnding::default())
-        .map_err(PersistError::ssh_serializing_error)?;
+        .map_err(KeyWriteErrorSource::from)
+        .map_err(with_key(&secret_key))?;
 
-    create_secret_file(key_path, ser_key.as_str()).await?;
+    create_secret_file(key_path, ser_key.as_str())
+        .await
+        .map_err(with_key(secret_key))?;
     debug!("Wrote key to {key_path:?}");
     Ok(())
 }
 
-async fn create_secret_file(file: &Path, content: &str) -> Result {
+fn with_key(key: &SecretKey) -> impl FnOnce(KeyWriteErrorSource) -> PersistError {
+    |e| PersistError::KeyWriteError {
+        source: e,
+        key: key.to_owned(),
+    }
+}
+
+async fn create_secret_file(file: &Path, content: &str) -> Result<(), KeyWriteErrorSource> {
     let mut parent = file.to_owned();
     if parent.pop() {
         fs::create_dir_all(parent.clone())
             .await
-            .map_err(for_file(parent))?
+            .map_err(writing_file(parent))?
     }
 
     let mut open_options = OpenOptions::new();
@@ -102,11 +150,11 @@ async fn create_secret_file(file: &Path, content: &str) -> Result {
 
     let mut open_file = (open_options.create(true).write(true).open(file))
         .await
-        .map_err(for_file(file.to_owned()))?;
+        .map_err(writing_file(file.to_owned()))?;
     open_file
         .write_all(content.as_bytes())
         .await
-        .map_err(for_file(file.to_owned()))?;
+        .map_err(writing_file(file.to_owned()))?;
     Ok(())
 }
 
@@ -141,8 +189,8 @@ mod tests {
     #[tokio::test]
     #[test_log::test]
     async fn it_generates_emphemeral_keys() {
-        let first_key = get_secret_key_from_ref(None).await;
-        let second_key = get_secret_key_from_ref(None).await;
+        let first_key = get_secret_key_from_option(None).await;
+        let second_key = get_secret_key_from_option(None).await;
         assert_ne!(first_key.to_bytes(), second_key.to_bytes());
     }
 
@@ -151,9 +199,8 @@ mod tests {
     async fn it_stores_different_keys() {
         let test_tmp_dir = TestTmpDir::create().await;
         let tmp_dir = test_tmp_dir.dir_str();
-        let first_key = get_secret_key(Some(format!("{tmp_dir}/iroh-secret-foo.pem").into())).await;
-        let second_key =
-            get_secret_key(Some(format!("{tmp_dir}/test/iroh-secret-bar.pem").into())).await;
+        let first_key = get_secret_key(format!("{tmp_dir}/iroh-secret-foo.pem").into()).await;
+        let second_key = get_secret_key(format!("{tmp_dir}/test/iroh-secret-bar.pem").into()).await;
         assert_ne!(first_key.to_bytes(), second_key.to_bytes());
     }
 
@@ -162,14 +209,12 @@ mod tests {
     async fn it_reuses_a_key_from_a_given_location() {
         let test_tmp_dir = TestTmpDir::create().await;
         let tmp_dir = test_tmp_dir.dir_str();
-        let first_key =
-            get_secret_key(Some(format!("{tmp_dir}/test/iroh-secret-foo.pem").into())).await;
+        let first_key = get_secret_key(format!("{tmp_dir}/test/iroh-secret-foo.pem").into()).await;
         let second_location = Some(format!("{tmp_dir}/test/iroh-secret-foo.pem").into())
             .or_else(|| default_persist_at("bar"));
         debug!("Second location: {second_location:?}");
-        let second_key = get_secret_key(second_location).await;
-        let third_key =
-            get_secret_key(Some(format!("{tmp_dir}/test/iroh-secret-bar.pem").into())).await;
+        let second_key = get_secret_key_from_option(second_location).await;
+        let third_key = get_secret_key(format!("{tmp_dir}/test/iroh-secret-bar.pem").into()).await;
         assert_eq!(first_key.to_bytes(), second_key.to_bytes());
         assert_ne!(first_key.to_bytes(), third_key.to_bytes());
     }
@@ -179,12 +224,25 @@ mod tests {
     async fn it_reuses_a_key_from_a_default_location() {
         let test_tmp_dir = TestTmpDir::create().await;
         let tmp_dir = test_tmp_dir.dir_str();
-        let first_key = get_secret_key(default_persist_at("foo")).await;
+        let first_key = get_secret_key_from_option(default_persist_at("foo")).await;
         let persist_at_foo = None::<PathBuf>.or_else(|| default_persist_at("foo"));
-        let second_key = get_secret_key_from_ref(persist_at_foo.as_ref()).await;
-        let third_key =
-            get_secret_key(Some(format!("{tmp_dir}/test/iroh-secret-bar.pem").into())).await;
+        let second_key = get_secret_key_from_option_ref(persist_at_foo.as_ref()).await;
+        let third_key = get_secret_key(format!("{tmp_dir}/test/iroh-secret-bar.pem").into()).await;
         assert_eq!(first_key.to_bytes(), second_key.to_bytes());
         assert_ne!(first_key.to_bytes(), third_key.to_bytes());
+    }
+
+    #[tokio::test]
+    #[test_log::test]
+    async fn it_uses_an_ephemeral_key_when_it_cannot_read_key() {
+        let test_tmp_dir = TestTmpDir::create().await;
+        let tmp_dir = test_tmp_dir.dir_str();
+        let file_path: PathBuf = format!("{tmp_dir}/iroh-secret-foo.pem").into();
+        fs::write(&file_path, "nonsense".as_bytes().to_owned())
+            .await
+            .unwrap();
+        let first_key = get_secret_key_from_ref(&file_path).await;
+        let second_key = get_secret_key_from_ref(&file_path).await;
+        assert_ne!(first_key.to_bytes(), second_key.to_bytes());
     }
 }
